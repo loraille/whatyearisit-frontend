04c6c36193193b6d3c05a7103e5772da8461f1d8
Laurent <lo.rail@proton.me>
2024-06-28 14:35:55 +0200

    wxcvxwcv

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ const urlFetch = 'https://whatyearisit-backend-gilt.vercel.app'
 
 
 
-fetch(`${urlFetch}/date`)
+fetch(`${urlFetch}/year`)
     .then(response => response.json())
     .then(data => {
         document.getElementById('year').textContent = data.year
